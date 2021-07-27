30235b55eb1d8e024726527f7de1b8e53729e575
ayelenperezdemendiola <a.perezdemendiola@gmail.com>
2021-07-27 17:55:26 +0200

    before delete sections

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Ancho de la ventana
 Mousemove en el body// o en el nav container
 Usar clientX (distancia del ratón horizontal)
+
+https://www.twitch.tv/videos/1084586204
--- js/app.js
+++ js/app.js
@@ -1,3 +1,64 @@
 'use strict';
 
-console.log('working again');
+const pointer = document.getElementById('mouse-circle');
+const navContainer = document.querySelector('.main__nav');
+const navList = document.querySelector('.main__nav__list');
+const navItem = document.querySelectorAll('.nav__list__item');
+const navItemLink = document.querySelectorAll('.nav__list__item--link');
+const mainNavContainer = document.querySelector('.main__nav__container');
+const menuSection = document.querySelectorAll('.main__section');
+const links = document.querySelectorAll('a');
+
+
+window.addEventListener('mousemove', mouseFollow);
+
+function mouseFollow(){
+	pointer.style.left = event.clientX - 20 + "px";
+	pointer.style.top = event.clientY - 20 + "px";
+}
+
+links.forEach((v, i) => {
+    links[i].addEventListener('mouseover', ()=>{
+        pointer.classList.add('pointerHover');
+    })
+    links[i].addEventListener('mouseout', ()=>{
+        pointer.classList.remove('pointerHover');
+    })
+})
+
+navItem.forEach((v, i) => {
+    navItem[i].addEventListener('mouseover', (e)=> {
+        navItem.forEach((v, i) => {
+            navItem[i].classList.remove('active');
+        })
+        navItem[i].classList.add('active');
+        if( i === 0) {
+            navList.style.transform = 'translateX(52vw)';
+            navList.style.transition = 'all 1s ease-in-out';
+        } else if ( i === 1) {
+            navList.style.transform = 'translateX(0)';
+            navList.style.transition = 'all 1s ease-in-out';
+        } else if ( i === 2) {
+            navList.style.transform = 'translateX(-52vw)';
+            navList.style.transition = 'all 1s ease-in-out';
+        }
+    })
+})
+
+navContainer.addEventListener('mouseout', ()=> {
+    navList.style.transform = 'translateX(0)';
+    navList.style.transition = 'all 1s ease-in-out';
+})
+
+navItemLink.forEach((v, i)=> {
+    navItemLink[i].addEventListener('click', ()=>{
+        navItemLink.forEach((v,i)=>{
+            navItemLink[i].classList.remove('active');
+            mainNavContainer.classList.toggle('active');
+            
+        })
+        navItemLink[i].classList.add('active');
+    })
+})
+
+
