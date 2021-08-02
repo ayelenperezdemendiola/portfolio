bce0eb79791dbd74bac6eac7303167f886e48f7b
ayelenperezdemendiola <a.perezdemendiola@gmail.com>
2021-08-02 10:26:22 +0200

    first review

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 'use strict';
-
+const body = document.querySelector('.body');
 const pointer = document.getElementById('mouse-circle');
-const navContainer = document.querySelector('.main__nav');
-const navList = document.querySelector('.main__nav__list');
-const navItem = document.querySelectorAll('.nav__list__item');
-const navItemLink = document.querySelectorAll('.nav__list__item--link');
-const mainNavContainer = document.querySelector('.main__nav__container');
-const menuSection = document.querySelectorAll('.main__section');
-const links = document.querySelectorAll('a');
-const navTrigger = document.querySelector ('.nav__trigger');
-const nav = document.querySelector('.header__nav');
+const navContainer = body.querySelector('.main__nav');
+const navList = body.querySelector('.main__nav__list');
+const navItem = body.querySelectorAll('.nav__list__item');
+const navItemLink = body.querySelectorAll('.nav__list__item--link');
+const mainNavContainer = body.querySelector('.main__nav__container');
+const menuSection = body.querySelectorAll('.main__section');
+const links = body.querySelectorAll('a');
+const navTrigger = body.querySelector ('.nav__trigger');
+const nav = body.querySelector('.header__nav');
 
 
 window.addEventListener('mousemove', mouseFollow);
@@ -19,10 +19,13 @@ function mouseFollow(){
 	pointer.style.top = event.clientY - 20 + "px";
 }
 
-navTrigger.addEventListener('click', () => {
-    nav.classList.toggle('open');
-    navTrigger.classList.toggle('open');
-})
+
+if(!body.classList.contains('body--home')){
+    navTrigger.addEventListener('click', () => {
+        nav.classList.toggle('open');
+        navTrigger.classList.toggle('open');
+    })
+}
 
 links.forEach((v, i) => {
     links[i].addEventListener('mouseover', ()=>{
@@ -33,29 +36,35 @@ links.forEach((v, i) => {
     })
 })
 
-navItem.forEach((v, i) => {
-    navItem[i].addEventListener('mouseover', (e)=> {
-        navItem.forEach((v, i) => {
-            navItem[i].classList.remove('active');
+if(body.classList.contains('body--home')){
+    navItem.forEach((v, i) => {
+        navItem[i].addEventListener('mouseover', (e)=> {
+            navItem.forEach((v, i) => {
+                navItem[i].classList.remove('active');
+            })
+            navItem[i].classList.add('active');
+            console.log(window.innerWidth);
+            if(window.innerWidth > 750){
+                if( i === 0) {
+                    navList.style.transform = 'translateX(52vw)';
+                    navList.style.transition = 'all 1s ease-in-out';
+                } else if ( i === 1) {
+                    navList.style.transform = 'translateX(0)';
+                    navList.style.transition = 'all 1s ease-in-out';
+                } else if ( i === 2) {
+                    navList.style.transform = 'translateX(-52vw)';
+                    navList.style.transition = 'all 1s ease-in-out';
+                }
+            }
         })
-        navItem[i].classList.add('active');
-        if( i === 0) {
-            navList.style.transform = 'translateX(52vw)';
-            navList.style.transition = 'all 1s ease-in-out';
-        } else if ( i === 1) {
-            navList.style.transform = 'translateX(0)';
-            navList.style.transition = 'all 1s ease-in-out';
-        } else if ( i === 2) {
-            navList.style.transform = 'translateX(-52vw)';
-            navList.style.transition = 'all 1s ease-in-out';
-        }
     })
-})
+    navContainer.addEventListener('mouseout', ()=> {
+        navList.style.transform = 'translateX(0)';
+        navList.style.transition = 'all 1s ease-in-out';
+    })
+}
 
-navContainer.addEventListener('mouseout', ()=> {
-    navList.style.transform = 'translateX(0)';
-    navList.style.transition = 'all 1s ease-in-out';
-})
+
 
 navItemLink.forEach((v, i)=> {
     navItemLink[i].addEventListener('click', ()=>{
